192e842c7f7b0c699f09172e9cff9d46e91d8d04
El-Luih <luisalejandro632@gmail.com>
2026-07-17 03:27:09 -0600

    OF COURSE IT IS A TYPO

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   root: 'src/',
 
   build: {
-    outDir: './docs',
+    outDir: '../docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
